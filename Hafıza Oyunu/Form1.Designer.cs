ced76aab999294ffbb5a1f306724ef972538456e
Gürkan Merter <grknmrtr6116@gmail.com>
2021-11-12 09:29:28 +0300

    Proje dosyası ekle.

---
+++ Hafıza Oyunu/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿
+namespace Hafıza_Oyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.BaslatButon = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.RadioKolay = new System.Windows.Forms.RadioButton();
+            this.RadioZor = new System.Windows.Forms.RadioButton();
+            this.RadioOrta = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.YenidenBasla = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // BaslatButon
+            // 
+            this.BaslatButon.Location = new System.Drawing.Point(562, 12);
+            this.BaslatButon.Name = "BaslatButon";
+            this.BaslatButon.Size = new System.Drawing.Size(120, 41);
+            this.BaslatButon.TabIndex = 0;
+            this.BaslatButon.Text = "Baslat";
+            this.BaslatButon.UseVisualStyleBackColor = true;
+            this.BaslatButon.Click += new System.EventHandler(this.BaslatButon_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(200, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Zorluk Seçiniz:";
+            // 
+            // RadioKolay
+            // 
+            this.RadioKolay.AutoSize = true;
+            this.RadioKolay.Location = new System.Drawing.Point(310, 19);
+            this.RadioKolay.Name = "RadioKolay";
+            this.RadioKolay.Size = new System.Drawing.Size(67, 24);
+            this.RadioKolay.TabIndex = 5;
+            this.RadioKolay.TabStop = true;
+            this.RadioKolay.Text = "Kolay";
+            this.RadioKolay.UseVisualStyleBackColor = true;
+            // 
+            // RadioZor
+            // 
+            this.RadioZor.AutoSize = true;
+            this.RadioZor.Location = new System.Drawing.Point(456, 21);
+            this.RadioZor.Name = "RadioZor";
+            this.RadioZor.Size = new System.Drawing.Size(53, 24);
+            this.RadioZor.TabIndex = 6;
+            this.RadioZor.TabStop = true;
+            this.RadioZor.Text = "Zor";
+            this.RadioZor.UseVisualStyleBackColor = true;
+            // 
+            // RadioOrta
+            // 
+            this.RadioOrta.AutoSize = true;
+            this.RadioOrta.Location = new System.Drawing.Point(383, 19);
+            this.RadioOrta.Name = "RadioOrta";
+            this.RadioOrta.Size = new System.Drawing.Size(59, 24);
+            this.RadioOrta.TabIndex = 7;
+            this.RadioOrta.TabStop = true;
+            this.RadioOrta.Text = "Orta";
+            this.RadioOrta.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 20);
+            this.label2.TabIndex = 8;
+            // 
+            // YenidenBasla
+            // 
+            this.YenidenBasla.Enabled = false;
+            this.YenidenBasla.Location = new System.Drawing.Point(717, 13);
+            this.YenidenBasla.Name = "YenidenBasla";
+            this.YenidenBasla.Size = new System.Drawing.Size(158, 41);
+            this.YenidenBasla.TabIndex = 9;
+            this.YenidenBasla.Text = "Yeniden Secim Yap";
+            this.YenidenBasla.UseVisualStyleBackColor = true;
+            this.YenidenBasla.Click += new System.EventHandler(this.YenidenBasla_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(29, 73);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(934, 674);
+            this.panel1.TabIndex = 10;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1235, 759);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.YenidenBasla);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.RadioOrta);
+            this.Controls.Add(this.RadioZor);
+            this.Controls.Add(this.RadioKolay);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BaslatButon);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BaslatButon;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton RadioKolay;
+        private System.Windows.Forms.RadioButton RadioZor;
+        private System.Windows.Forms.RadioButton RadioOrta;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button YenidenBasla;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Hafıza Oyunu/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Hafıza_Oyunu
+{
+    public partial class Form1 : Form
+    {
+        int[] Dizi = new int[36];
+        string[] ResimDizi = new string[18];
+        Button[,] btndizi = new Button[6, 6];
+        
+        int ZorlukKatsayısı;
+        List<string> BulunanResimler = new List<string>();
+        int TiklanmaKontrol = 0;
+        Button ilkButton = new Button(); 
+        Button ikinciButton = new Button();
+     
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BaslatButon_Click(object sender, EventArgs e)
+        {
+
+            if (RadioKolay.Checked == true)
+            {
+                label2.Text = "Kolay Seçtiniz";
+                BulunanResimler.Clear();
+                ZorlukKatsayısı = 2;
+                timer1.Interval = 1000;
+            }
+            else if (RadioOrta.Checked == true)
+            {
+                label2.Text = "Orta Seçtiniz";
+                BulunanResimler.Clear();
+                ZorlukKatsayısı = 4;
+                timer1.Interval = 500;
+            }
+            else if (RadioZor.Checked == true)
+            {
+                label2.Text = "Zor Seçtiniz";
+                BulunanResimler.Clear();
+                ZorlukKatsayısı = 6;
+                timer1.Interval = 200;
+            }
+
+            ResimDiziOlustur();
+            DiziOlustur(ZorlukKatsayısı * ZorlukKatsayısı);
+            ButonOlustur(ZorlukKatsayısı);
+            BaslatButon.Enabled = false;
+            RadioKolay.Enabled = false;
+            RadioOrta.Enabled = false;
+            RadioZor.Enabled = false;
+            YenidenBasla.Enabled = true;
+            
+        }
+
+        private void YenidenBasla_Click(object sender, EventArgs e)
+        {
+            BaslatButon.Enabled = true;
+            RadioKolay.Checked = false;
+            RadioOrta.Checked = false;
+            RadioZor.Checked = false;
+            RadioKolay.Enabled = true;
+            RadioOrta.Enabled = true;
+            RadioZor.Enabled = true;
+            YenidenBasla.Enabled = false;
+            label2.Text = "";
+            panel1.Controls.Clear();
+
+        }
+
+        public void ButonOlustur(int x)
+        {
+
+            int DiziDekiSayi, index = 0;
+
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < x; j++)
+                {
+
+                    DiziDekiSayi = Dizi[index];
+                    index++;
+                    btndizi[i, j] = new Button();
+                    btndizi[i, j].Size = new Size(150, 110);
+                    btndizi[i, j].BackgroundImage = global::Hafıza_Oyunu.Properties.Resources.kartarkasi;
+                    btndizi[i, j].BackgroundImageLayout = ImageLayout.Stretch;
+                    btndizi[i, j].FlatStyle = FlatStyle.Flat;
+                    btndizi[i, j].Name = DiziDekiSayi.ToString();
+                    btndizi[i, j].Click += new EventHandler(tiklanmaController);
+                    btndizi[i, j].Location = new Point(150 * (j), 110 * (i));
+                    panel1.Controls.Add(btndizi[i, j]);
+                }
+            }
+        }
+
+        private void tiklanmaController(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+            
+            button.BackgroundImage = Image.FromFile(ResimDizi[Convert.ToInt32(button.Name)]);
+            button.BackgroundImageLayout = ImageLayout.Stretch;
+            button.Enabled = false;
+            TiklanmaKontrol++;
+            if (TiklanmaKontrol < 2)
+            {
+                ilkButton = button;
+
+            }
+            else if (TiklanmaKontrol == 2)
+            {
+                TiklanmaKontrol = 0;
+                ikinciButton = button;
+                if (ilkButton.Name==ikinciButton.Name)
+                {
+                    BulunanResimler.Add(ikinciButton.Name);
+                    SonucKontrol();
+                }
+                else
+                {
+                    ButonlariKapat();
+                    timer1.Start();
+                }
+
+            }
+
+        }
+
+        public void YanlisFotoyuKapat()
+        {
+            ilkButton.BackgroundImage = global::Hafıza_Oyunu.Properties.Resources.kartarkasi;
+            ikinciButton.BackgroundImage = global::Hafıza_Oyunu.Properties.Resources.kartarkasi;
+        }
+        public void ButonlariAc()
+        {
+            for (int i = 0; i < ZorlukKatsayısı; i++)
+            {
+                for (int j = 0; j < ZorlukKatsayısı; j++)
+                {
+                    if (!BulunanResimler.Contains(btndizi[i, j].Name))
+                    {
+                        btndizi[i, j].Enabled = true;
+                    }
+
+                }
+            }
+        }
+
+        public void ButonlariKapat()
+        {
+
+            for (int i = 0; i < ZorlukKatsayısı; i++)
+            {
+                for (int j = 0; j < ZorlukKatsayısı; j++)
+                {
+                    btndizi[i, j].Enabled = false;
+                }
+            }
+        }
+        public void SonucKontrol()
+        {
+            if (BulunanResimler.Count == (ZorlukKatsayısı * ZorlukKatsayısı) / 2)
+            {
+                MessageBox.Show("Tebrikler Oyunu Bitirdiniz.");
+            }
+        }
+
+        public void DiziOlustur(int y)
+        {
+
+            Random random = new Random();
+            for (int i = 0; i < y; i++)
+            {
+                if (i == 0)
+                {
+                    Dizi[i] = random.Next(0, (y / 2));
+                }
+                else
+                {
+                    int sayac = 0;
+                    int randomsayi2 = random.Next(0, (y / 2));
+                    for (int j = 0; j < i; j++)
+                    {
+                        if (Dizi[j] == randomsayi2)
+                        {
+                            sayac++;
+                        }
+                    }
+                    if (sayac < 2)
+                    {
+                        Dizi[i] = randomsayi2;
+                    }
+                    else
+                    {
+                        i = i - 1;
+                    }
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            YanlisFotoyuKapat();
+            ButonlariAc();
+        }  
+        public void ResimDiziOlustur()
+        {
+
+            ResimDizi[0] = @"C:\Users\MONSTER\source\repos\Hafıza Oyunu\Hafıza Oyunu\Resimler\aslan.png";
+            ResimDizi[1] = @"C:\Users\MONSTER\source\repos\Hafıza Oyunu\Hafıza Oyunu\Resimler\at.png";
+            ResimDizi[2] = @"C:\Users\MONSTER\source\repos\Hafıza Oyunu\Hafıza Oyunu\Resimler\ayı.png";
+            ResimDizi[3] = @"C:\Users\MONSTER\source\repos\Hafıza Oyunu\Hafıza Oyunu\Resimler\balina.png";
+            ResimDizi[4] = @"C:\Users\MONSTER\source\repos\Hafıza Oyunu\Hafıza Oyunu\Resimler\black jaguar.png";
+            ResimDizi[5] = @"C:\Users\MONSTER\source\repos\Hafıza Oyunu\Hafıza Oyunu\Resimler\civciv.png";
+            ResimDizi[6] = @"C:\Users\MONSTER\source\repos\Hafıza Oyunu\Hafıza Oyunu\Resimler\davuk.png";
+            ResimDizi[7] = @"C:\Users\MONSTER\source\repos\Hafıza Oyunu\Hafıza Oyunu\Resimler\geyik.png";
+            ResimDizi[8] = @"C:\Users\MONSTER\source\repos\Hafıza Oyunu\Hafıza Oyunu\Resimler\inek.png";
+            ResimDizi[9] = @"C:\Users\MONSTER\source\repos\Hafıza Oyunu\Hafıza Oyunu\Resimler\kaplan.png";
+            ResimDizi[10] = @"C:\Users\MONSTER\source\repos\Hafıza Oyunu\Hafıza Oyunu\Resimler\karides.png";
+            ResimDizi[11] = @"C:\Users\MONSTER\source\repos\Hafıza Oyunu\Hafıza Oyunu\Resimler\kartal.png";
+            ResimDizi[12] = @"C:\Users\MONSTER\source\repos\Hafıza Oyunu\Hafıza Oyunu\Resimler\Kedi.jpg";
+            ResimDizi[13] = @"C:\Users\MONSTER\source\repos\Hafıza Oyunu\Hafıza Oyunu\Resimler\köpke.png";
+            ResimDizi[14] = @"C:\Users\MONSTER\source\repos\Hafıza Oyunu\Hafıza Oyunu\Resimler\kurt.png";
+            ResimDizi[15] = @"C:\Users\MONSTER\source\repos\Hafıza Oyunu\Hafıza Oyunu\Resimler\panda.png";
+            ResimDizi[16] = @"C:\Users\MONSTER\source\repos\Hafıza Oyunu\Hafıza Oyunu\Resimler\penguen.png";
+            ResimDizi[17] = @"C:\Users\MONSTER\source\repos\Hafıza Oyunu\Hafıza Oyunu\Resimler\tilki.png";
+        }
+
+    }
+}
